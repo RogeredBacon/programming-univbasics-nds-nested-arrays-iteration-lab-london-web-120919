7abb1c8b914a025fe060c54e4830a5d5f1627279
James Inglis <jinglis12@googlemail.com>
2019-11-29 18:40:00 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,7 +25,7 @@ def find_greater_pair(src)
   new_array = []
   while row_index < src.count do
     src[row_index].sort
-    new_array.push(src[row_index][1])
+    new_array.push(src[row_index][-1])
     row_index += 1
   end
   new_array
